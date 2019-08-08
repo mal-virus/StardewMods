4a528dc0cdaf5b6b30359acbe524c3138f31724b
Malcolm Thirus <(none)>
2019-08-08 16:01:14 -0400

    Configuration file added dependencies changed to allow usage across operating systems solution file updated to maintain formatting

---
+++ TwitchCompatibility/ModConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace TwitchCompatibility {
+    class ModConfig {
+        public string AuthToken { get; set; }
+        public string BotUsername { get; set; }
+        public string Channel { get; set; }
+        public string ClientID { get; set; }
+    }
+}
